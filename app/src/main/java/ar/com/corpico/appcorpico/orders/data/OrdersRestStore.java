32e18a61b0821c8cf433c84580e32ff034a96bb7
mrnrdgz <mrnrdgz@gmail.com>
2017-04-26 20:48:15 -0300

    Anda agregar etapas en estado pendiente (asignar a cuadrilla)

--- app/src/main/java/ar/com/corpico/appcorpico/orders/data/OrdersRestStore.java
+++ app/src/main/java/ar/com/corpico/appcorpico/orders/data/OrdersRestStore.java
@@ -22,8 +22,18 @@ public class OrdersRestStore implements OrderStore {
     private  static final ArrayList<Etapa> mFakeRestEtapa3 = new ArrayList<>();
     private  static final ArrayList<Etapa> mFakeRestEtapa4 = new ArrayList<>();
     private  static final ArrayList<Etapa> mFakeRestEtapa5 = new ArrayList<>();
+    private  static final ArrayList<Etapa> mFakeRestEtapa6 = new ArrayList<>();
+    private  static final ArrayList<Etapa> mFakeRestEtapa7 = new ArrayList<>();
+    private  static final ArrayList<Etapa> mFakeRestEtapa8 = new ArrayList<>();
+    private  static final ArrayList<Etapa> mFakeRestEtapa9 = new ArrayList<>();
+    private  static final ArrayList<Etapa> mFakeRestEtapa10 = new ArrayList<>();
     static {
         mFakeRestEtapa.add(new Etapa("2017-01-23T00:00:00.000-03:00","Pendiente","Nada"));
+        mFakeRestEtapa6.add(new Etapa("2017-01-23T00:00:00.000-03:00","Pendiente","xxx"));
+        mFakeRestEtapa7.add(new Etapa("2017-01-23T00:00:00.000-03:00","Pendiente","zzzz"));
+        mFakeRestEtapa8.add(new Etapa("2017-01-23T00:00:00.000-03:00","Pendiente","www"));
+        mFakeRestEtapa9.add(new Etapa("2017-01-23T00:00:00.000-03:00","Pendiente","sss"));
+        mFakeRestEtapa10.add(new Etapa("2017-01-23T00:00:00.000-03:00","Pendiente","yyy"));
 
         mFakeRestEtapa1.add(new Etapa("2017-01-23T00:00:00.000-03:00","Pendiente","Nada"));
         mFakeRestEtapa1.add(new Etapa("2017-01-25T00:00:00.000-03:00","Culminada", "Todo"));
@@ -47,14 +57,14 @@ public class OrdersRestStore implements OrderStore {
     static {
         mFakeRestOrder.add(new Order("839127", "Eléctrico", "2", "Retiro de Medidor", "Por Morosidad",mFakeRestEtapa3,"15514", "1", "Luisa Gonzalez", "Pasaje Rivero 957", "General Pico","","35.6630S", "63.7608W", "Nada"));
         mFakeRestOrder.add(new Order("839128", "Eléctrico", "3", "Cambio de Medidor", "Trabado",mFakeRestEtapa, "22814", "1", "Jorgelina Rodriguez", "Calle 531", "General Pico","", "35.6562S", "63.7537W", "Algo"));
-        mFakeRestOrder.add(new Order("839129", "Eléctrico", "4", "Colocacion de Medidor", "Suministro Nuevo", mFakeRestEtapa, "24429", "7", "Gustavo Turienzo", "Calle 29", "General Pico","","35.6657S", "63.7494W", "Todo"));
-        mFakeRestOrder.add(new Order("839130", "Eléctrico", "4", "Retiro de Medidor", "Solicitud del Cliente", mFakeRestEtapa, "55472", "1", "Gonzalo Fernandez", "Calle 18", "General Pico","","35.6601S", "63.7690W", "Siempre"));
-        mFakeRestOrder.add(new Order("839131", "Eléctrico", "1", "Retiro de Medidor", "Por Morosidad", mFakeRestEtapa, "40462", "2", "Antonella Privitera", "Calle 28", "General Pico","","35.6538S", "63.7528W", "Nunca"));
-        mFakeRestOrder.add(new Order("839132", "Eléctrico", "2", "Retiro de Medidor", "Por Morosidad",mFakeRestEtapa3, "17495", "1", "Juan Perez", "Pasaje Rivero 957", "General Pico","","35.6629S", "63.7476W", "Nada"));
+        mFakeRestOrder.add(new Order("839129", "Eléctrico", "4", "Colocacion de Medidor", "Suministro Nuevo", mFakeRestEtapa6, "24429", "7", "Gustavo Turienzo", "Calle 29", "General Pico","","35.6657S", "63.7494W", "Todo"));
+        mFakeRestOrder.add(new Order("839130", "Eléctrico", "4", "Retiro de Medidor", "Solicitud del Cliente", mFakeRestEtapa7, "55472", "1", "Gonzalo Fernandez", "Calle 18", "General Pico","","35.6601S", "63.7690W", "Siempre"));
+        mFakeRestOrder.add(new Order("839131", "Eléctrico", "1", "Retiro de Medidor", "Por Morosidad", mFakeRestEtapa8, "40462", "2", "Antonella Privitera", "Calle 28", "General Pico","","35.6538S", "63.7528W", "Nunca"));
+        mFakeRestOrder.add(new Order("839132", "Eléctrico", "2", "Retiro de Medidor", "Por Morosidad",mFakeRestEtapa9, "17495", "1", "Juan Perez", "Pasaje Rivero 957", "General Pico","","35.6629S", "63.7476W", "Nada"));
         mFakeRestOrder.add(new Order("839133", "Eléctrico", "3", "Cambio de Medidor", "Solic. Energia Prepaga",mFakeRestEtapa4, "6377", "1", "Rodrigo Nieto", "Calle 531", "General Pico","","35.6788S", "63.7530W", "Algo"));
-        mFakeRestOrder.add(new Order("839134", "Eléctrico", "4", "Colocacion de Medidor", "Regularizacion de Deuda", mFakeRestEtapa, "44345", "1", "Jose Ferrando", "Calle 29", "General Pico","","35.6678S", "63.7555W", "Todo"));
+        mFakeRestOrder.add(new Order("839134", "Eléctrico", "4", "Colocacion de Medidor", "Regularizacion de Deuda", mFakeRestEtapa10, "44345", "1", "Jose Ferrando", "Calle 29", "General Pico","","35.6678S", "63.7555W", "Todo"));
         mFakeRestOrder.add(new Order("839135", "Eléctrico", "4", "Retiro de Medidor", "Solicitud del Cliente",mFakeRestEtapa5, "42352", "1", "Fabio Gomez", "Calle 18", "General Pico","","35.6810S", "63.7491W", "Siempre"));
-        mFakeRestOrder.add(new Order("839136", "Eléctrico","1", "Retiro de Medidor", "Por Morosidad",mFakeRestEtapa3, "20484", "1", "Maria Gallo", "Calle 28", "General Pico","","35.6598S", "63.7498W", "Nunca"));
+        mFakeRestOrder.add(new Order("839136", "Eléctrico","1", "Retiro de Medidor", "Por Morosidad",mFakeRestEtapa2, "20484", "1", "Maria Gallo", "Calle 28", "General Pico","","35.6598S", "63.7498W", "Nunca"));
     }
 
     @Override
