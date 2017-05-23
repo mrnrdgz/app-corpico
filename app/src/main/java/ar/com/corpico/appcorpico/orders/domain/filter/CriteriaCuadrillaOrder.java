0c7e2fb8d9e6a1835f3395f5d392a576474d3ff1
mrnrdgz <mrnrdgz@gmail.com>
2017-05-23 12:53:38 -0300

    ver el filtrado de las cuadrillas

---
+++ app/src/main/java/ar/com/corpico/appcorpico/orders/domain/filter/CriteriaCuadrillaOrder.java
@@ -0,0 +1,42 @@
+package ar.com.corpico.appcorpico.orders.domain.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.com.corpico.appcorpico.orders.domain.entity.Etapa;
+import ar.com.corpico.appcorpico.orders.domain.entity.Order;
+import ar.com.corpico.appcorpico.orders.domain.entity.Tipo;
+
+/**
+ * Created by Administrador on 26/01/2017.
+ */
+public class CriteriaCuadrillaOrder implements Criteria<Order> {
+    private final String cuadrilla;
+
+    public CriteriaCuadrillaOrder(String tipo) {
+        this.cuadrilla=tipo;
+    }
+
+    @Override
+    public List<Order> match(List<Order> orders) {
+        List<Order> filteredOrders = new ArrayList<>();
+        CriteriaCuadrillaTipo filteredTipo = new CriteriaCuadrillaTipo(cuadrilla);
+
+        /*for (Order order : orders) {
+            for(int x=0;x<filteredTipo.size();x++) {
+                System.out.println(filteredTipo.get(x));
+            }
+            if (order.getTipo().equals(cuadrilla)) {
+                filteredOrders.add(order);
+            }
+        }*/
+
+        return filteredOrders;
+    }
+
+
+    @Override
+    public Object toSql() {
+        return "select ... from order where state = " + cuadrilla;
+    }
+}
