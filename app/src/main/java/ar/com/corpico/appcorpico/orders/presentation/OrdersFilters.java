a9786a021fbd072717b06cfd7878b2078007c413
mrnrdgz <mrnrdgz@gmail.com>
2017-07-03 13:01:06 -0300

    oooo

--- app/src/main/java/ar/com/corpico/appcorpico/orders/presentation/OrdersFilters.java
+++ app/src/main/java/ar/com/corpico/appcorpico/orders/presentation/OrdersFilters.java
@@ -1,5 +1,7 @@
 package ar.com.corpico.appcorpico.orders.presentation;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,15 +13,23 @@ import android.widget.LinearLayout;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import ar.com.corpico.appcorpico.R;
+import ar.com.corpico.appcorpico.orders.OrderPendienteActivity;
 
 public class OrdersFilters extends AppCompatActivity {
     private List<String> mTipoTrabajo = new ArrayList<>();
     private List<String> mTipoTrabajoSelected = new ArrayList<>();
     private String mEstado;
     private CheckBox Tipo;
+
+    public interface TipoTrabajoListener{
+        void SetTipoTrabajo(ArrayList<String> mTipoTrabajo);
+    }
+    TipoTrabajoListener listener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,8 +45,10 @@ public class OrdersFilters extends AppCompatActivity {
             Tipo = new CheckBox(this);
             Tipo.setText(mTipoTrabajo.get(i));
             if(mTipoTrabajoSelected!=null){
-                if(mTipoTrabajoSelected.contains(mTipoTrabajo.get(i))){
-                    Tipo.isChecked();
+                for(int j=0; j< mTipoTrabajoSelected.size(); j++) {
+                    if(Tipo.getText().equals(mTipoTrabajoSelected.get(j))){
+                        Tipo.setChecked(true);
+                    }
                 }
             }
             Tipo.setLayoutParams(
@@ -44,20 +56,20 @@ public class OrdersFilters extends AppCompatActivity {
                             LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             Tipo.setPadding(16,16,16,16);
             seccionTiposTrabajo.addView(Tipo);
-            Tipo.setOnClickListener(new android.view.View.OnClickListener() {
-                @Override
-                public void onClick(android.view.View v) {
-                    if (Tipo.isChecked()) {
-                        // Guardar indice seleccionado
-                        mTipoTrabajoSelected.add((Tipo.getText()).toString());
-                    } else if (mTipoTrabajoSelected.contains((Tipo.getText()).toString())) {
-                        // Remover indice sin selección
-                        mTipoTrabajoSelected.remove(Integer.valueOf((Tipo.getText()).toString()));
-                    }
-                }
-            });
         }
-
+        Tipo.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                if (Tipo.isChecked()) {
+                    // Guardar indice seleccionado
+                    mTipoTrabajoSelected.add((String) Tipo.getText());
+                } else if (mTipoTrabajoSelected.contains(Tipo.getText())){
+                    // Remover indice sin selección
+                    //Arrays.binarySearch(mTipoTrabajoSelected.toArray(),Tipo.getText());
+                    mTipoTrabajoSelected.remove(Arrays.binarySearch(mTipoTrabajoSelected.toArray(),Tipo.getText()));
+                }
+            }
+        });
         setToolbar();
     }
     private void setToolbar() {
@@ -87,12 +99,27 @@ public class OrdersFilters extends AppCompatActivity {
                 onBackPressed();
                 break;
             case R.id.action_aplicar:
-
+                //listener.SetTipoTrabajo();
+                OrderPendienteActivity mAct = new OrderPendienteActivity();
+                mAct.SetTipoTrabajo(mTipoTrabajo);
                 break;
             case R.id.action_limpiar:
 
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (TipoTrabajoListener) context;
+
+        } catch (ClassCastException e) {
+            throw new ClassCastException(
+                    context.toString() +
+                            " no implementó OnSimpleDialogListener");
+        }
     }
 }
